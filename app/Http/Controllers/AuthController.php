48f1232f1a17a112f5fcda3b3a4c3cc3bf60f31e
Farditbee <farhandito15@gmail.com>
2025-02-19 13:31:11 +0700

    Oke

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
     }
     public function login(Request $request)
     {
-        $credentials = $request->only('username', 'password');
+        $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
