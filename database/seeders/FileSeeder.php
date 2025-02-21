19d524b106011b7cfb865bc42d07220c28a08112
Farditbee <farhandito15@gmail.com>
2025-02-21 11:40:12 +0700

    update : fileSeeder

--- database/seeders/FileSeeder.php
+++ database/seeders/FileSeeder.php
@@ -14,34 +14,25 @@ class FileSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
-
-        for ($i = 1; $i <= 3; $i++) {
-            DB::table('file')->insert([
-                'id_katalog' => $i,
-                'file_name' => $faker->boolean(70) ? 'desain_' . $i . '.jpg' : null // 70% ada file, 30% kosong
-            ]);
-        }
-
-        for ($i = 1; $i <= 3; $i++) {
-            DB::table('file')->insert([
-                'id_katalog' => $i,
-                'file_name' => $faker->boolean(70) ? 'desain_' . $i . '.jpg' : null // 70% ada file, 30% kosong
-            ]);
-        }
-
-        for ($i = 1; $i <= 3; $i++) {
-            DB::table('file')->insert([
-                'id_katalog' => $i,
-                'file_name' => $faker->boolean(70) ? 'desain_' . $i . '.jpg' : null // 70% ada file, 30% kosong
-            ]);
-        }
-
-        for ($i = 1; $i <= 3; $i++) {
-            DB::table('file')->insert([
-                'id_katalog' => $i,
-                'file_name' => $faker->boolean(70) ? 'desain_' . $i . '.jpg' : null // 70% ada file, 30% kosong
-            ]);
-        }
+        DB::table('file')->insert([
+            'id_katalog' => 1,
+            'file_name'  => 'desain_1.jpg'
+        ]);
+        DB::table('file')->insert([
+            'id_katalog' => 2,
+            'file_name'  => 'desain_1.jpg'
+        ]);
+        DB::table('file')->insert([
+            'id_katalog' => 3,
+            'file_name'  => 'desain_1.jpg'
+        ]);
+        DB::table('file')->insert([
+            'id_katalog' => 1,
+            'file_name'  => 'desain_1.jpg'
+        ]);
+        DB::table('file')->insert([
+            'id_katalog' => 1,
+            'file_name'  => 'desain_1.jpg'
+        ]);
     }
 }
