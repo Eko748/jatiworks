13d99bf9f8ee0bc8c6dadf98cbba5d14a154be8b
Farditbee <farhandito15@gmail.com>
2025-02-21 13:38:07 +0700

    update: datakatalog + category

--- app/Http/Controllers/KatalogController.php
+++ app/Http/Controllers/KatalogController.php
@@ -31,24 +31,17 @@ class KatalogController extends Controller
         $meta['orderBy'] = $request->ascending ? 'asc' : 'desc';
         $meta['limit'] = $request->has('limit') && $request->limit <= 30 ? $request->limit : 30;
 
-        $query = Katalog::query();
-
-        $query->with(['role'])->orderBy('id', $meta['orderBy']);
+        $query = Katalog::with(['category'])
+            ->orderBy('id', $meta['orderBy']);
 
         if (!empty($request['search'])) {
             $searchTerm = trim(strtolower($request['search']));
-
-            $query->where(function ($query) use ($searchTerm) {
-                $query->orWhereRaw("LOWER(item_name) LIKE ?", ["%$searchTerm%"]);
-
-            });
+            $query->whereRaw("LOWER(item_name) LIKE ?", ["%$searchTerm%"]);
         }
 
         if ($request->has('startDate') && $request->has('endDate')) {
             $startDate = $request->input('startDate');
             $endDate = $request->input('endDate');
-
-            // Lakukan filter berdasarkan tanggal
             $query->whereBetween('id', [$startDate, $endDate]);
         }
 
@@ -61,37 +54,38 @@ class KatalogController extends Controller
             'total_pages'  => $data->lastPage()
         ];
 
-        $data = [
-            'data' => $data->items(),
-            'meta' => $paginationMeta
-        ];
-
-        if (empty($data['data'])) {
-            return response()->json([
-                'status_code' => 400,
-                'errors' => true,
-                'message' => 'Empty Data'
-            ], 400);
-        }
-
-        $mappedData = collect($data['data'])->map(function ($item) {
+        $mappedData = $data->map(function ($item) {
             return [
-                'id' => $item['id'],
-                'item_name' => $item->item_name,
-                'material' => $item->material,
-                'length' => $item->length,
-                'width' => $item->width,
-                'height' => $item->height,
-                'desc' => $item->desc,
+                'id'         => $item->id,
+                'item_name'  => $item->item_name,
+                'material'   => $item->material,
+                'length'     => $item->length,
+                'width'      => $item->width,
+                'height'     => $item->height,
+                'desc'       => $item->desc,
+                'category' => $item->category->map(function ($category) {
+                    return [
+                        'id_category' => $category->id,
+                        'name_category'        => $category->name_category,
+                    ];
+                }),
             ];
         });
 
+        if ($mappedData->isEmpty()) {
+            return response()->json([
+                'status_code' => 400,
+                'errors'      => true,
+                'message'     => 'Empty Data'
+            ], 400);
+        }
+
         return response()->json([
-            'data' => $mappedData,
+            'data'       => $mappedData,
             'status_code' => 200,
-            'errors' => true,
-            'message' => 'Success',
-            'pagination' => $data['meta']
+            'errors'     => false,
+            'message'    => 'Success',
+            'pagination' => $paginationMeta
         ], 200);
     }
 }
